9b9490ee4ba11071305d333ad6cc7069c731e65f
Sandra-beep <sandee.rivas@gmail.com>
2021-02-25 22:26:28 +0100

    test style

--- index.php
+++ index.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "style.css" />
     <title>Startpage</title>
 </head>
 <body>
-    <a href="views/sign-up.php">Sign up</a>
-    <a href="views/login.php">Login</a>
+   <div class ="start">
+      <div class ="signup-link">
+         <a href="views/sign-up.php">Sign up</a>
+      </div>
+      <div class ="login-link">
+         <a href="views/login.php">Login</a>
+      </div>
+   </div>
 </body>
 </html>
