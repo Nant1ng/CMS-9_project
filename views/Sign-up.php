1d142bded78ab01a0d40ae9bf6bbc1bc27250b17
Sandra-beep <sandee.rivas@gmail.com>
2021-02-27 18:11:32 +0100

    la till stylesheet på diverse

--- views/Sign-up.php
+++ views/Sign-up.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign up</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+<title>Sign up</title>
 </head>
 <?php
 session_start();
