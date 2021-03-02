3f6147e0d97536ad6117e0ea9b3ae83c397fab43
Sandra-beep <sandee.rivas@gmail.com>
2021-03-02 22:36:52 +0100

    relativt klar med index.html

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
-    <link rel = "stylesheet" type = "text/css" href = "css/index.css" />
 
     <title>Startpage</title>
 </head>
@@ -13,11 +12,11 @@
 <?php
     include_once('includes/header.php');
 ?>
-   <div class ="start">
-      <h4>Welcome! Login here<h4>
+   <div class ="startpage-container">
+      <p class = "welcomeLogin">Welcome! Login here</p>
       <a id="login" href="views/login.php">Login</a>
-      <h4>Or if your new here, sign up here<h4>
-       <a id="signup" href="views/sign-up.php">Sign up</a></h4>
+      <p class = "welcomeLogin">Or if your new here, sign up!</p>
+      <p><a id="signup" href="views/sign-up.php">Sign up</a></p>
    </div>
 
 </body>
