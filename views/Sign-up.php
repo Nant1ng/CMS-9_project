b208e37c0abcac1432a0df5a03a7d4ed2d169cf9
Nant1ng <andressantana99@hotmail.se>
2021-03-04 19:24:24 +0100

    Check if username or email is already taken)

--- views/Sign-up.php
+++ views/Sign-up.php
@@ -7,56 +7,87 @@
     <link rel = "stylesheet" type = "text/css" href = "css/style.css" />
 <title>Sign up</title>
 </head>
+
 <?php
 session_start();
 include('../includes/database_connection.php');
-try{
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// För registrering 
-if(isset($_POST["sign-up"]))
-{
-    $regUsername = $_POST['regUsername'];
-    $regPassword = $_POST['regPassword'];
-    $salt = "siahbndjiasnidja12893183s9300";
-    $regPassword = md5($regPassword.$salt);
-    $regEmail = $_POST['regEmail'];
-    $regFname = $_POST['regFname'];
-    $regLname = $_POST['regLname'];
-    
 
-    if(empty($_POST["regUsername"]) || empty($_POST["regPassword"]) || empty($_POST["regEmail"]) || empty($_POST["regFname"]) || empty($_POST["regLname"]))
-    {
-        $message = '<label>All fields must be filled</label>';
+$regUsername = $regEmail = "";
+$regUsername_error = $regEmail_error = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $sql = "SELECT userID FROM users WHERE username = :username_IN";
+
+    if($stm = $pdo->prepare($sql)){
+        $stm->bindParam(":username_IN", $param_regUsername, PDO::PARAM_STR);
+        $param_regUsername = trim($_POST["regUsername"]);
+
+        if($stm->execute()){
+            if($stm->rowCount() == 1){
+                $regUsername_error = "This username is already taken!";
+            } else{
+                $regUsername = trim($_POST["regUsername"]);
+            }
+        }
     }
-    else
-    {
+    $sql = "SELECT userID FROM users WHERE email = :email_IN";
 
-        $sql = "INSERT INTO users (username,password,email,fname,lname) VALUES(:username_IN, :password_IN, :email_IN, :fname_IN, :lname_IN)";
-        $stm = $pdo->prepare($sql);
-        $stm->bindParam(':username_IN', $regUsername);
-        $stm->bindParam(':password_IN', $regPassword);
-        $stm->bindParam(':email_IN', $regEmail);
-        $stm->bindParam(':fname_IN', $regFname);
-        $stm->bindParam(':lname_IN', $regLname);
+    if($stm = $pdo->prepare($sql)){
+        $stm->bindParam(":email_IN", $param_regEmail, PDO::PARAM_STR);
+        $param_regEmail = trim($_POST["regEmail"]);
 
-          if($stm->execute()){
-            //echo "Register success";
-              header("location:../index.php");
-          }else {
-              echo "Something went wrong try again";
-          }
-  
+        if($stm->execute()){
+            if($stm->rowCount() == 1){
+                $regEmail_error = "This email is already taken!";
+            } else{
+                $regEmail = trim($_POST["regEmail"]);
+            }
+        }
     }
-}      
-}  
-catch(PDOException $error)  
-{  
-    $message = $error->getMessage();  
+    if(empty($regUsername_error) && empty($regEmail_error)){
+        try{
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if(isset($_POST["sign-up"]))
+        {
+            $regUsername = $_POST['regUsername'];
+            $regPassword = $_POST['regPassword'];
+            $salt = "siahbndjiasnidja12893183s9300";
+            $regPassword = md5($regPassword.$salt);
+            $regEmail = $_POST['regEmail'];
+            $regFname = $_POST['regFname'];
+            $regLname = $_POST['regLname'];
+        
+            if(empty($_POST["regUsername"]) || empty($_POST["regPassword"]) || empty($_POST["regEmail"]) || empty($_POST["regFname"]) || empty($_POST["regLname"]))
+            {
+                $message = '<label>All fields must be filled</label>';
+            }
+            else
+            {
+                $sql = "INSERT INTO users (username,password,email,fname,lname) VALUES(:username_IN, :password_IN, :email_IN, :fname_IN, :lname_IN)";
+                $stm = $pdo->prepare($sql);
+                $stm->bindParam(':username_IN', $regUsername);
+                $stm->bindParam(':password_IN', $regPassword);
+                $stm->bindParam(':email_IN', $regEmail);
+                $stm->bindParam(':fname_IN', $regFname);
+                $stm->bindParam(':lname_IN', $regLname);
+        
+                  if($stm->execute()){
+                    echo "Register success";
+                     header("location:../index.php");
+                  }else {
+                      echo "Something went wrong try again";
+                  }
+            }
+        }      
+        }  
+        catch(PDOException $error)  
+        {  
+            $message = $error->getMessage();  
+        }
+    }
 }
-
 ?>
 
-<!-- för error meddelandet -->
+<!-- För error meddelandet -->
 <?php  
     if(isset($errorMessage)){  
         echo '<label>'.$errorMessage.'</label>';  
@@ -71,14 +102,16 @@ catch(PDOException $error)
         <h3>Sign up</h3>
         <pre>
             <form method="post" class ="signup-form">
-                <label hidden>First name:</label>
+                <label>First name:</label>
                 <input type="text" name="regFname" placeholder = "First name">
                 <label>Last name:</label>
                 <input type="text" name="regLname" placeholder = "Last name">
                 <label>Email:</label>
                 <input type="text" name="regEmail" placeholder = "Email">
+                <span><?php echo $regEmail_error; ?></span>
                 <label>Username:</label>
                 <input type="text" name="regUsername" placeholder = "Username">
+                <span><?php echo $regUsername_error; ?></span>
                 <label>Password:</label>
                 <input type="password" name="regPassword"placeholder = "Password">
                 <input type="submit" name="sign-up" value="Sign-up">
