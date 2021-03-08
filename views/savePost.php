f52076ed64e74c22d0ac61c3735f2ea179488d93
Sandra-beep <sandee.rivas@gmail.com>
2021-03-08 14:09:13 +0100

    image upload fixat på savePost.php

--- views/savePost.php
+++ views/savePost.php
@@ -1,37 +1,9 @@
-<?php
-session_start();
-include '../includes/database_connection.php';
-
-
-$title = $_POST['title'];
-$description = $_POST['description'];
-$imageUrl = $_POST['imageUrl'];
-// $imageUrl = $_POST['imageUpload'];
-$category = $_POST['category'];
-$date = $_POST['date'];
-
-$sql = "INSERT INTO posts (title,description,imageUrl,category,date) VALUES(:title_IN, :description_IN, :imageUrl_IN, :category_IN, :date_IN)";
-$stm = $pdo->prepare($sql);
-$stm->bindParam(':title_IN', $title);
-$stm->bindParam(':description_IN', $description);
-$stm->bindParam(':imageUrl_IN', $imageUrl);
-// $stm->bindParam(':imageUpload_IN', $imageUrl);
-$stm->bindParam(':category_IN', $category);
-$stm->bindParam(':date_IN', $date);
-
-if($stm->execute()) {
-    header("location:../loggedin.php");
-}else{
-    echo "Something went wrong try again";
-}
-?>
-
 <!-- Här laddar vi upp bilder från datorn :)  -->
 
 <?php
-$upload_dir = "uploads/";
+$upload_dir = "../image/uploads/";
 $target_file = $upload_dir . basename($_FILES['imageToUpload']['name']);
-$fileType = strtolower(pathinfo($target_file, PATHFINDER_EXTENSION));
+$fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 if(isset($_POST['submit'])){
     $check = getimagesize($_FILES['imageToUpload']['top_name']);
@@ -55,6 +27,42 @@ if($fileType != "png" && $fileType != "gif" && $fileType != "jpg" && $fileType !
     echo "You can only upload PNG, GIF, JPG or JPEG";
     die;
 }
+if(move_uploaded_file($_FILES['imageToUpload']['tmp_name'], $target_file)){
+    echo "File uploaded succesfully";
+}else {
+    die;
+};
 
 ?>
+
+
+
+
+<?php
+session_start();
+include '../includes/database_connection.php';
+
+
+$title = $_POST['title'];
+$description = $_POST['description'];
+$imageUrl = $_POST['imageUrl'];
+$category = $_POST['category'];
+$date = $_POST['date'];
+
+$sql = "INSERT INTO posts (title,description,imageUrl,category,date) VALUES(:title_IN, :description_IN, :imageUrl_IN, :category_IN, :date_IN)";
+$stm = $pdo->prepare($sql);
+$stm->bindParam(':title_IN', $title);
+$stm->bindParam(':description_IN', $description);
+$target_file = "/PhpOvn/CMS-9_project/image/" . $target_file;
+$stm->bindParam(':imageUrl_IN', $target_file);
+$stm->bindParam(':category_IN', $category);
+$stm->bindParam(':date_IN', $date);
+
+if($stm->execute()) {
+    header("location:../loggedin.php");
+}else{
+    echo "Something went wrong try again";
+}
+?>
+
 
